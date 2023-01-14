a0f03d186b4d6f4d2a62d634a3d1fca8c436958c
Arsalan Warsi <arsalanwarsi27@gmail.com>
2023-01-14 15:57:05 +0500

    update url

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import routes from "../router";
 import MainSection from "./MainSection";
+import TopBar from "./TopBar";
 
 export default function Dashboard(props) {
   return (
@@ -12,6 +13,7 @@ export default function Dashboard(props) {
         <Navbar route={routes} />
         <div className="main-body">
           {/* <Outlet /> */}
+          <TopBar />
           <MainSection />
         </div>
       </div>
